4040974ceaaae337766eab0027b6d5e019400d7c
Kaspar <kasparkivi12@gmail.com>
2020-01-14 11:35:20 +0200

    kaspi

--- book.php
+++ book.php
@@ -18,20 +18,38 @@ try {
 $stmt = $pdo->prepare('SELECT * FROM books WHERE id= :id');
 $stmt->execute(['id' => $_GET['id']]);
 $book = $stmt->fetch();
-var_dump($book);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php
-    echo $book['title'];
-    ?>
+    <title><?php echo $book['title'];?>
     </title>
 </head>
 <body>
     
+<p> <h1>Pealkiri </h1>
+<?php echo $book['title'];?>
+</p>    
+<div class="info">
+<p> <h1>Ilmumis aasta </h1>
+<?php echo $book['release_date'];?>
+</p>  
+<p> <h1>Tüüp</h1>
+<?php echo $book['type'];?>
+</p>  
+<p> <h1>Keel</h1>
+<?php echo $book['language'];?>
+</p>
+<p> <h1>Hind</h1>
+<?php echo $book['stock_saldo'];?>
+</p>
+</div>
+<p> 
+<?php class="summary"> echo $book['summary'];?>
+</p>
 </body>
 </html>
